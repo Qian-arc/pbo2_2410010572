39a08913d5a9c0cc9505fe50002cd5736e38acf9
Rizqian Ardy Rahman <rizqianardyr@gmail.com>
2026-09-24 13:13:12 +0800

    P01: proyek pertama HaloPBO2

---
+++ p01-halo-java/src/main/java/id/ac/uniska/pbo2/p01/KartuMahasiwa.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.uniska.pbo2.p01;
+
+/**
+ *
+ * @author ASUS TUF
+ */
+public class KartuMahasiwa {
+     public static void main (String[] args){
+        //Dekarasi variabel dengan tipe data yag sesuai
+        String nama = "Rizqian Ardy Rahman";
+        String npm = "2410010572";
+        String Prodi = "Teknik Informatika";
+        int semester = 5;
+        String alasan = "ingin memperdalam  program berbasis objek dan pngembangan aplikasi";
+        
+        System.out.println("=====================================");
+        System.out.println("KARTU MAHASISWA PBO 2");
+        System.out.println("=====================================");
+        System.out.println("Nama             : " + nama);
+        System.out.println("NPM              : " + npm);
+        System.out.println("Prodi            : " + Prodi);
+        System.out.println("semester         : " + semester);
+        System.out.println("Alasan           : " + alasan);
+        System.out.println("=====================================");
+    }
+}
